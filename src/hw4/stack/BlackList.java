6cb7a6534972974fe62fa118c763fdb294adcb02
Tanya Gensitskaya <gensitskaya.tanya@gmail.com>
2016-10-07 19:50:10 +0300

    hw4 change3

--- src/hw4/stack/BlackList.java
+++ src/hw4/stack/BlackList.java
@@ -1,37 +1,24 @@
 package hw4.stack;
 
+import java.util.ArrayList;
+
 public class BlackList {
-	private Object[] objectArray = new Object[2];
+	private ArrayList<Object> objectArray = new ArrayList<>();
 
 	public void add(Object obj) {
-		for (int i = 0; i < objectArray.length; i++) {
-			if (objectArray[i] == null) {
-				objectArray[i] = obj;
-				return;
-			}
-		}
-		rebuildArray();
-		add(obj);
+		objectArray.add(obj);
 	}
 
-	private void rebuildArray() {
-		int coefficient = 2;
-		Object[] newArray = new Object[objectArray.length * coefficient];
-		System.arraycopy(objectArray, 0, newArray, 0, objectArray.length);
-		objectArray = newArray;
-	}
-
-	public boolean checkObjectInBlackList(Object obj){
+	public boolean checkObjectInBlackList(Object obj) {
 		for (Object e : objectArray) {
-			if (e!=null && e.equals(obj)) {
+			if (e != null && e.equals(obj)) {
 				return true;
 			}
 		}
-		 return false;
+		return false;
 	}
 
-
-	public Object[] getObjectArray() {
+	public ArrayList<Object> getObjectArray() {
 		return objectArray;
 	}
 }
